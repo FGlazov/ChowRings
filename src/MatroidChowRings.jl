54b784fc628c24b627c22e8fbd27abd71857f524
Fedor Glazov <glazov@math.tu-berlin.de>
2021-10-27 19:29:13 +0200

    Massive commit: Compute direct sum decomposition.
    Forgot to commit this... still a TODO version with some edge
    case errors, and "easy" wins for performance. Still it is possible
    to compute the decomposition for small matroids (up to 12 elements
    seems to work almost instantly.)

--- src/MatroidChowRings.jl
+++ src/MatroidChowRings.jl
@@ -3,8 +3,72 @@ module MatroidChowRings
 using Oscar;
 const pm = Polymake;
 
+struct MChowRing
+    chow_ring # TODO Add types here.
+    projection
+    indeterminates::Vector{fmpq_mpoly}
+    matroid::pm.BigObject
+end
 
-function matroid_chow_ring(matroid::pm.BigObject)
+function direct_sum_decomp(matroid::pm.BigObject, matroid_element::Int64)
+    direct_sum_decomp(matroid_chow_ring(matroid), matroid_element)
+end
+
+function direct_sum_decomp(chow_ring::MChowRing, matroid_element::Int64)
+    # TODO: Check bounds of matroid element?
+
+    # TODO: matroid.COLOOPS seems to be broken. Ask why?
+    matroid = chow_ring.matroid
+
+    first_term = matroid_chow_ring(pm.matroid.deletion(matroid, matroid_element))
+
+    ground_set = Polymake.Set(range(0, matroid.N_ELEMENTS - 1,step=1))
+    flats = matroid.LATTICE_OF_FLATS.FACES
+    flats = pm.@pm common.convert_to{IncidenceMatrix}(flats)
+    proper_flats = flats[2:pm.size(flats, 1)-1, 1:pm.size(flats,2)]
+    n_proper_flats = pm.size(proper_flats, 1)
+
+    flat_canidates = []
+    # TODO: I'm sure this can be done MUCH faster using the IncidenceMatrix stucture.
+    # That way, we can turn a "O(2*n)" pass into a "O(n) + O(log n)" pass
+    for flat_index in 1:n_proper_flats
+        proper_flat = pm.row(proper_flats, flat_index)
+        if Polymake.in(matroid_element, proper_flat)
+            push!(flat_canidates, proper_flat)
+        end
+    end
+    flat_canidates = Set(flat_canidates)
+
+    # TODO: To make this quicker, we could parallelize this task.
+    second_term = []
+    for flat_index in 1:n_proper_flats
+        proper_flat = pm.row(proper_flats, flat_index)
+        if !Polymake.in(matroid_element, proper_flat)
+            proper_flat_copy = copy(proper_flat)
+            push!(proper_flat_copy, matroid_element)
+            if proper_flat_copy in flat_canidates
+                matroid_1 = pm.matroid.contraction(matroid, proper_flat_copy)
+                matroid_2 = pm.matroid.deletion(matroid, set_complement(ground_set, proper_flat_copy))
+
+                push!(second_term, (matroid_chow_ring(matroid_1), matroid_chow_ring(matroid_2)))
+            end
+        end
+    end
+
+    coloops = matroid.DUAL.LOOPS
+    is_coloop = pm.in(matroid_element, coloops)
+    coloop_term = nothing
+    if is_coloop
+        coloop_term = first_term
+    end
+
+    #TODO Also return a function which represents the cannonical isomorphism.
+
+    first_term, second_term, coloop_term
+
+end
+
+function matroid_chow_ring(matroid::pm.BigObject)::MChowRing
     if pm.type_name(matroid) != "Matroid"
         throw(ArgumentError("BigObject is not a matroid."))
     end
@@ -14,8 +78,12 @@ function matroid_chow_ring(matroid::pm.BigObject)
 
     # Technically non-empty proper flats. First element is either empty or the whole set, last element is the other of the two.
     proper_flats = flats[2:pm.size(flats, 1)-1, 1:pm.size(flats,2)]
-
     base_ring, indeterminates = generate_base_ring(proper_flats)
+
+    if length(indeterminates) == 0
+        return MChowRing(base_ring, nothing, indeterminates, matroid)
+    end
+
     type_i_polynomials = generate_type_i_ideal(proper_flats, indeterminates, matroid)
     type_j_polynomials = generate_type_j_ideal(proper_flats, indeterminates)
     generators = vcat(type_i_polynomials, type_j_polynomials)
@@ -23,8 +91,8 @@ function matroid_chow_ring(matroid::pm.BigObject)
     chow_modulus = ideal(base_ring, generators)
     # Add std(ideal) to compute standard basis?
     chow_ring, projection = quo(base_ring, chow_modulus)
-    chow_ring, projection, indeterminates
 
+    MChowRing(chow_ring, projection, indeterminates, matroid)
 end
 
 
@@ -55,13 +123,14 @@ function augmented_matroid_chow_ring(matroid::pm.BigObject)
     # Add std(ideal) to compute standard basis?
     chow_ring, projection = quo(base_ring, chow_modulus)
     chow_ring, projection, matroid_element_vars, flat_vars
-
 end
 
 
 function generate_base_ring(proper_flats)
     variable_names = create_flat_variables_names(proper_flats)
-
+    # TODO: This doesn't seem to work well when it represents the trivial ring.
+    #       Printing a trivial ring generated here causes errors.
+    #       Perhaps a different way to create the trivial ring?
     PolynomialRing( QQ, variable_names);
 end
 
@@ -100,6 +169,7 @@ function generate_type_i_ideal(proper_flats, indeterminates, matroid)
     n = matroid.N_ELEMENTS
     n_proper_flats = pm.size(proper_flats, 1)
     ideal_polynomials = Vector{fmpq_mpoly}()
+
     for i in 1:n
         for j in i+1:n
             ij_polynomial = indeterminates[1] - indeterminates[1] # TODO: Better way to create zero polynomial?
@@ -184,6 +254,12 @@ function are_sets_incomparable(a, b)
     return !is_subset(a,b) && !is_subset(b,a)
 end
 
+function are_sets_equal(a,b)
+
+
+    is_subset(a,b) && is_subset(b,a)
+end
+
 function is_subset(a, b)
     for element in a
         if !Polymake.in(element, b)
@@ -191,6 +267,14 @@ function is_subset(a, b)
         end
     end
     true
+end
+
+function set_complement(ground_set, to_complement)
+    result = copy(ground_set)
+    for i in to_complement
+        delete!(result, i)
+    end
+    result
 end
 
 end
